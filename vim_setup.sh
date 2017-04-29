53bb2056153b0547f3dcc4faa8e12ce2f2c15136
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2017-04-28 20:34:01 -0500

    Ensure that autoload dir exists in .vim before downloading pathogen

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pathogen="https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
 
+mkdir vim/.vim/autoload
 curl "$pathogen" > vim/.vim/autoload/pathogen.vim
